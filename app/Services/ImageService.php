a42c1f2bbb68d666c5a0919cdb82f14949b54717
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-09-20 12:14:46 -0300

    modificando_fila_'queue'_para_pilha_'stack'_para_que_a_primeira_alteracao_feita_seja_a_utima_a_ser_desfeita_e_vice-versa

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -8,15 +8,17 @@ use Illuminate\Support\Facades\Storage;
 
 class imageService implements ImageServiceInterface {       // implements ImageServiceInterface  implementando interfaçe 'ImageSerrviceInterface'
 
-    private $rollbackQueue = null;      // fila nula
+    private $rollbackStack = null;      // pilha nula
     
     public function deleteImageFromDisk($imageUrl): bool {
+        echo '<- deleteImageFromDisk <br>';
         $imagePath = str_replace(asset('storage/'), '', $imageUrl);
         Storage::disk('public')->delete($imagePath);     // se houver um erro no upload da imagem 'catch' dispara um 'delete'
         return true;
     }
  
     public function deleteImageDatabaseImage($databaseImage): bool {
+        echo '<- deleteImageDatabaseImage <br>';
         if ($databaseImage) {
             $databaseImage->delete();
             return true;
@@ -24,17 +26,27 @@ class imageService implements ImageServiceInterface {       // implements ImageS
         return false;
     }
 
-    public function rollback($databaseImage) {      // método 'rollback' é responsável por deleta a imagem do localstorage e do banco de dados
-        if(!empty($this->rollbackQueue)) {      // se a fila não estiver vazia
-            foreach($this->rollbackQueue as $interaction) {     // percorrendo a fila rollbackQueue
-                $method = $interaction['method'];
-                $params = $interaction['params'];
+    public function rollback() {      // método 'rollback' é responsável por deleta a imagem do localstorage e do banco de dados
+        while(!empty($this->rollbackStack)) {
+            $rollbackAction = array_pop($this->rollback);        // pegando a ação de rollback
 
-                if(method_exists($this, $method)) {     // se o metodo existir
-                    call_user_func_array([$this,$method], $params);     // chama o método e os parâmetros do mesmo
-                }
+            $method = $rollbackAction['method'];
+            $paams = $rollbackAction['params'];
+            if(method_exists($this, $method)) {
+                call_user_func_array([$this, $method], $params);
             }
         }
+
+        // if(!empty($this->rollbackQueue)) {      // se a fila não estiver vazia
+        //     foreach($this->rollbackQueue as $interaction) {     // percorrendo a fila rollbackQueue
+        //         $method = $interaction['method'];
+        //         $params = $interaction['params'];
+
+        //         if(method_exists($this, $method)) {     // se o metodo existir
+        //             call_user_func_array([$this,$method], $params);     // chama o método e os parâmetros do mesmo
+        //         }
+        //     }
+        // }
     }
 
     public function storeNewImage($image, $title): image {        // método 'storareNewImage' cria a imagem e grava no banco de dados
@@ -47,6 +59,7 @@ class imageService implements ImageServiceInterface {       // implements ImageS
     }
 
    private function storeImageInDisk($image): string {           // método salva a imagem de forma publica e retorna um caminho para a mesma
+        echo '-> storeImageInDisk <br>';
        $imageName = $image->store('uploads', 'public');             // caminho
        $url = asset('storage/' . $imageName);
        $this->addToRollbackQueue('deleteImageFromDisk', [$url]);        // passando url da imagem que será deletada
@@ -54,6 +67,7 @@ class imageService implements ImageServiceInterface {       // implements ImageS
    } 
 
    private function storeImageInDataBase($title, $url): Image {     // 'private' transform em métodos espeçificos do 'ImageService'
+        echo '-> storeImageInDataBase <br>';
        $image = Image::create([     // salva a 'images' no banco de dados
            'title'=> $title,
            'url'=> $url
@@ -63,8 +77,8 @@ class imageService implements ImageServiceInterface {       // implements ImageS
        return $image;
    }
 
-   private function addToRollbackQueue() {      // método 'addToRollbackQueue' adicionará o método que será executado e os parâmetros desse método
-        $this->rollbackQueue[] = [
+   private function addToRollbackQueue($method, $params = []) {      // método 'addToRollbackQueue' adicionará o método que será executado e os parâmetros desse método
+        $this->rollbackStack[] = [
             'method' => $method,
             'params' => $params
         ];
